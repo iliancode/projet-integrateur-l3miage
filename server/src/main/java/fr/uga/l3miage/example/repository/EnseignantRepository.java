66dd47702457f195627b9bee63896983983510f8
l3miage-benaisil <benaissailian@outlook.fr>
2023-04-26 10:36:28 +0200

    fin service enseignant

--- server/src/main/java/fr/uga/l3miage/example/repository/EnseignantRepository.java
+++ server/src/main/java/fr/uga/l3miage/example/repository/EnseignantRepository.java
@@ -1,12 +1,9 @@
 package fr.uga.l3miage.example.repository;
 
+import fr.uga.l3miage.example.models.Enseignant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import fr.uga.l3miage.example.models.Enseignant;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,10 +12,8 @@ public interface EnseignantRepository extends JpaRepository<Enseignant,Long> {
 
     Optional<Enseignant> findByMail(final String mail);
     Optional<Enseignant> findByPseudo(final String pseudo);
+
     int deleteByMail(final String mail);
-
-    Optional<Enseignant> findById(final long id);
-
     int deleteById(final long id);
 
 }
--- server/src/main/java/fr/uga/l3miage/example/service/EnseignantService.java
+++ server/src/main/java/fr/uga/l3miage/example/service/EnseignantService.java
@@ -7,6 +7,8 @@ import fr.uga.l3miage.example.request.CreateEnseignantRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @RequiredArgsConstructor
 public class EnseignantService {
@@ -14,9 +16,39 @@ public class EnseignantService {
     private final EnseignantComponent enseignantComponent;
     private final EnseignantMapper enseignantMapper;
 
-    public void createEnseignant( final CreateEnseignantRequest createEnseignantRequest){
-        Enseignant newEnseignant = enseignantMapper.dtoToEntity(createEnseignantRequest);
- 
+    public void createEnseignant( final CreateEnseignantRequest createEnseignantRequest) throws Exception {
+        Enseignant newEnseignant = enseignantMapper.toEntity(createEnseignantRequest);
+
+        if(newEnseignant.getMail().length() !=0){
+            if(newEnseignant.getPseudo().length() != 0){
+                if(newEnseignant.getMdp().length() >9){
+                    try{
+                        enseignantComponent.createEnseignant(newEnseignant);
+                    } catch (Exception e) {
+                        throw new Exception("erreur a la creation de l'enseignant");
+                    }
+                }else throw new Exception("mdp size <=9 ");
+            }else throw new Exception("pseudo size = 0 ");
+        }else throw new Exception("mail size = 0 ");
     }
+
+    @Transactional
+    public void deleteEnseignantByMail(String mail) throws Exception {
+        try {
+            enseignantComponent.deleteEnseignantByMail(mail);
+        } catch ( Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    @Transactional
+    public void deleteEnseignantById(long id) throws Exception {
+        try {
+            enseignantComponent.deleteEnseignantById(id);
+        } catch ( Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
 
 }
