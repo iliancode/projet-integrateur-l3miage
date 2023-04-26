45c2998b1d6945d5d3c5b8280e976b0ef1762156
l3miage-adomobia <adil.adomo@gmail.com>
2023-04-26 15:11:50 +0200

    Service / composant / mapper De Reponse OK

--- server/src/main/java/fr/uga/l3miage/example/component/ReponseComponent.java
+++ server/src/main/java/fr/uga/l3miage/example/component/ReponseComponent.java
@@ -18,7 +18,31 @@ public class ReponseComponent {
     private final ReponseRepository reponseRepository;
     private final ReponseMapper reponseMapper;
 
-    public void createReponse(final Reponse reponse) throws DescriptionAlreadyExistException {
+    //CREATION D'UNE REPONSE
+    public void createReponse(final Reponse reponse) throws Exception {
+        // on verifie qu'elle ne soit pas vide
+        if(reponse.getLabel() != ""){
+            // on verifie que la question n'existe pas déja
+            if(reponseRepository.findByLabel(reponse.getLabel()).isPresent()) {
+                throw new Exception(String.format("La question %s existe déjà en BD.", reponse.getLabel()));
+                }
+            reponseRepository.save(reponse);
+            } else throw new Exception("La nouvelle réponse est vide ");
 
+        }
+    //Suppression d'une reponse
+    public void deleteReponse(final String label) throws Exception{
+        int deleted = reponseRepository.deleteByLabel(label);
+        if(deleted > 1)
+            throw new Exception("Comment c'est possible sachant quil ne peut pas avoir 2 reponses avec le meme label");
+        else if(deleted == 0)
+            throw new Exception("La question a supprimé n'existe pas ");
     }
+
+    //TROUVER UNE REPONSE
+    public Reponse getReponse(final String label) throws Exception{
+        return reponseRepository.findByLabel((label))
+                .orElseThrow(()-> new Exception("Aucune réponse n'a été trouve"));
+    }
+
 }
