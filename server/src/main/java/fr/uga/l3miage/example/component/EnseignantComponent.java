95cc9d75b8aafe7d996e928cc3696d80c294454f
l3miage-benaisil <benaissailian@outlook.fr>
2023-04-26 09:59:04 +0200

    enseignant service pas fini, commit pour merge avec request

--- server/src/main/java/fr/uga/l3miage/example/component/EnseignantComponent.java
+++ server/src/main/java/fr/uga/l3miage/example/component/EnseignantComponent.java
@@ -1,6 +1,7 @@
 package fr.uga.l3miage.example.component;
 
 import fr.uga.l3miage.example.mapper.EnseignantMapper;
+import fr.uga.l3miage.example.models.Enseignant;
 import fr.uga.l3miage.example.repository.EnseignantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,23 @@ public class EnseignantComponent {
     private final EnseignantRepository enseignantRepository;
     private final EnseignantMapper enseignantMapper;
 
-    public void deleteEnseignant(final long id) {}
-
-
+    //test de creation d'un enseignant
+    public void createEnseignant(final Enseignant enseignant) throws Exception{
+        if(enseignant.getMail().isEmpty() == false){
+            if(enseignantRepository.findByMail(enseignant.getMail()).isPresent()){
+                throw new Exception("Mail deja existant dans la base de données");
+            }else{
+                if(enseignantRepository.findByPseudo(enseignant.getPseudo()).isPresent()){
+                    throw new Exception("Pseudo deja existant dans la base de données");
+                }
+                else{
+                    if(enseignant.getMdp().isEmpty()){
+                        throw new Exception("Le mot de passe ne peut pas etre vide");
+                    }else{
+                        enseignantRepository.save(enseignant);
+                    }
+                }
+            }
+        }else throw new Exception("Mail non renseigné");
+    }
 }
