75ad031d32453d59f8e2a158e16126b0332ee8cf
l3miage-benaisil <benaissailian@outlook.fr>
2023-04-27 10:42:28 +0200

    endpoint enseignant non fonctionnel pour merge

---
+++ rest-api/src/main/java/fr/uga/l3miage/example/endpoint/EnseignantEndpoint.java
@@ -0,0 +1,22 @@
+package fr.uga.l3miage.example.endpoint;
+
+import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Enseignant tag")
+@CrossOrigin
+@RestController
+@RequestMapping("enseignants/")
+public interface EnseignantEndpoint {
+    @Operation(description = "Récupérer le DTO de l'entité enseignant qui a pour id celui passé en paramètre")
+    @ApiResponse(responseCode = "200", description = "Renvoie le DTO de l'entité enseignant demandée",
+            content = @Content(schema = @Schema(implementation = EnseignantDTO.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
+
+}
