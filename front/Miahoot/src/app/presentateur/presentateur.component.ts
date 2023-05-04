e209207f7f488be67ae2ee8c4d59b39ea5b258fe
l3miage-adomobia <adil.adomo@gmail.com>
2023-05-04 17:03:13 +0200

    affichage miahoots et question du miahoot fonctionnel

--- front/Miahoot/src/app/presentateur/presentateur.component.ts
+++ front/Miahoot/src/app/presentateur/presentateur.component.ts
@@ -1,5 +1,30 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {DataService} from "../service/data.service";
+import {PresentationService} from "../service/presentation.service";
+import {BehaviorSubject, Observable} from "rxjs";
+
+interface Reponse {
+  id?: string;
+  label: string;
+  estValide: boolean;
+}
+
+interface Question {
+  id?: string;
+  label: string;
+  reponses: Reponse[];
+}
+
+interface Miahoot {
+  id?: string;
+  nom: string;
+  questions: Question[];
+}
+interface Enseignant {
+  pseudo : String,
+  mail : String,
+  mdp : String
+}
 
 @Component({
   selector: 'app-presentateur',
@@ -9,13 +34,59 @@ import {DataService} from "../service/data.service";
 })
 export class PresentateurComponent implements OnInit{
 
-  constructor(private ds : DataService) {}
+  enseignant : Enseignant = <Enseignant>{pseudo: 'adil', mail:'lpb@gmail.com', mdp : 'trop fort'};
+  miahoots: Miahoot[] = [];
+  miahoot : Miahoot;
+  public readonly question_courante: BehaviorSubject<Question | null>;
+  indexQuestionCourante = 0;
 
-  ngOnInit() : void {
+
+  constructor(private ps : PresentationService) {
+    this.miahoot = this.miahoots[0];
+    this.question_courante = new BehaviorSubject<Question | null>(null);
+
+  }
+
+  questionSuivante() {
+    this.indexQuestionCourante++;
+    if (this.indexQuestionCourante < this.miahoot.questions.length) {
+      this.question_courante.next(this.miahoot.questions[this.indexQuestionCourante]);
+    }
+  }
+
+  ngOnInit() {
+      this.ps.getMiahootsOfEnseignant('adil@gmail.com')
+      .then(miahoots => {
+        this.miahoots = miahoots;
+        this.miahoot = this.miahoots[0];
+        this.question_courante.next(this.miahoot.questions[this.indexQuestionCourante]);
+        console.log(this.miahoots);
+        console.log(this.miahoot);
+        console.log(this.question_courante);
+
+
+      });
+
+
+
+  }
+  }
+
+/**
+  async ngOnInit() {
+
     this.ds.getEnseignant("adil@gmail.com")
       .then(enseignant => console.log(enseignant))
       .catch(erreur =>console.log("pas d'enseignant trouve avec cet email"));
+
+
+    this.ds.createEnseignant(this.enseignant)
+      .then(enseignant => console.log(enseignant))
+      .catch(erreur =>console.log("Le post marche passssss"));
+
+    this.miahoots = await this.ds.getMiahootsOfEnseignant('adil@gmail.com');
+  console.log(this.miahoots);
+
   }
+     **/
 
-
-}
